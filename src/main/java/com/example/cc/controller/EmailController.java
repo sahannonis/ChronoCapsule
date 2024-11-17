928acc629230321d5a5f23b5cd542daccca3fa3b
sahannonis <34228337+sahannonis@users.noreply.github.com>
2024-11-17 12:30:56 -0800

    Update EmailController.java

--- src/main/java/com/example/cc/controller/EmailController.java
+++ src/main/java/com/example/cc/controller/EmailController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cc.model.EmailRequest;
 
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api/email")
 public class EmailController {
